012117ae788be00ef0ce76a1d68b7bb717737e31
siraz015 <siraj015213@gmail.com>
2023-06-07 11:00:33 +0600

    Route setup

--- src/Route/Route.jsx
+++ src/Route/Route.jsx
@@ -1,23 +1,23 @@
-// import {
-//     createBrowserRouter,
-// } from "react-router-dom";
-// import Main from "../Layout/Main";
-// import Home from "../components/Home/Home/Home";
-// import Login from "../components/Login/Login";
+import {
+    createBrowserRouter,
+} from "react-router-dom";
+import Main from "../Layout/Main";
+import Home from "../components/Home/Home/Home";
+import Login from "../components/Login/Login";
 
-// export const router = createBrowserRouter([
-//     {
-//         path: "/",
-//         element: <Main></Main>,
-//         children: [
-//             {
-//                 path: '/',
-//                 element: <Home></Home>
-//             },
-//             {
-//                 path: '/login',
-//                 element: <Login></Login>
-//             }
-//         ]
-//     },
-// ]);
+export const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            }
+        ]
+    },
+]);
